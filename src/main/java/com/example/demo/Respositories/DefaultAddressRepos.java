0f4b0e337f320fbb4af2ccffc556ff62a20647f0
huy <96323388+vvgvgcxgcgc@users.noreply.github.com>
2024-01-18 20:31:07 +0700

    Delete src directory

--- src/main/java/com/example/demo/Respositories/DefaultAddressRepos.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Respositories;
-
-import com.example.demo.Domain.Defaultaddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DefaultAddressRepos extends JpaRepository<Defaultaddress, Long> {
-
-}
--- src/main/java/com/example/demo/Respositories/Product_OrderRepos.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Respositories;
-
-import com.example.demo.Domain.Product_Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Product_OrderRepos extends JpaRepository<Product_Order, Long> {
-}
